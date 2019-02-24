300afd6890990e3dd1d6241bec2b4cf21f584f6b
joaaoeu <hello@joaao.eu>
2019-02-24 00:52:35 -0300

    Added signin + SessionController + auth and guest middlewares

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,19 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
+const SessionController = require('./app/controllers/SessionController')
 const UserController = require('./app/controllers/UserController')
 
-routes.get('/signup', UserController.create)
+routes.get('/', guestMiddleware, SessionController.create)
+routes.post('/signin', SessionController.store)
+
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.use('/app', authMiddleware)
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
 
 module.exports = routes
